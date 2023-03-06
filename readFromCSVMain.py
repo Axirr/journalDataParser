a9ec724dbcb122a01dc8d595e162bfb42ab88c53
Axirr <sherlock.scott@gmail.com>
2023-03-06 13:35:49 -0700

    Fix improperly named constant.

--- readFromCSVMain.py
+++ readFromCSVMain.py
@@ -1,4 +1,4 @@
-from src.globalConstants import UPDATE_ALL_SQL
+from src.globalConstants import ADD_SQL_ARG
 from src.readCSVtoSQL import readCSVtoSQLMain
 from sys import argv
 
@@ -7,6 +7,6 @@ def main(addOnly):
 
 if __name__ == "__main__":
     addOnly = True
-    if len(argv) > 1 and UPDATE_ALL_SQL in argv:
+    if len(argv) > 1 and ADD_SQL_ARG in argv:
         addOnly = False
     main(addOnly)
